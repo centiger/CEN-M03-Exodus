9d85d335b8a69e584dc140b11a3bd8db25d75e2e
centiger <centiger2@gmail.com>
2026-06-03 10:05:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-exodus-wilderness-v115';
+const CACHE='cen-exodus-wilderness-v116';
 const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./assets/exodus-wilderness-bg.png','./icons/icon-192.png','./icons/icon-512.png','./hubs/index.html','./hubs/js/app.js','./hubs/data/hubs.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cen-exodus-wilderness-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
